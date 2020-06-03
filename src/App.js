ec47edcb978d96194700d3772fdcb9eb8050239f
AndersonDevUsr <megaswat12@gmail.com>
2020-06-03 10:55:30 -0500

    -Listar personas -Eliminar personas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListaPersonasContainer from './Containers/ListaPersonasContainer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <ListaPersonasContainer />    
+    </div>
+  );
+}
+
+export default App
